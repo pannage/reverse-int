9c39e82b88c5217740451eaf0c88daf95862f4bc
Tatsiana <pannage@gmail.com>
2020-10-05 19:51:27 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if (n>0) {
+        let nStr = n + ''
+        return nStr.split('').reverse().join('')*1
+
+    } else {
+        let nStr = n*-1 + ''
+        return nStr.split('').reverse().join('')*1
+
+    }
+
 }
